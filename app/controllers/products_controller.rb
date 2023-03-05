869a3633d5528fdef4820561a2fc15478bbaa8e7
helmut cardenas <cardenas.hcconstrucciones@gmail.com>
2023-03-05 16:38:37 -0300

    creadas las instancias para que usuarios ingresen productos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,5 +12,20 @@ class ProductsController < ApplicationController
 
     end
 
+    def create
+        @product = Product.new(product_params)
+
+        if @product.save
+            redirect_to products_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def product_params
+        params.require(:product).permit(:title, :description, :price) #solo estos atributos ingresan en la BD
+    end
 
 end
