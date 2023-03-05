c171bb441f17fd97a09730295bf5ffbdb6d7a342
helmut cardenas <cardenas.hcconstrucciones@gmail.com>
2023-03-05 15:41:52 -0300

    creada la ruta para new

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -6,11 +6,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   # end
 
   test 'render a list of products' do
+    get product_path
 
+    assert_response :success #esperamos respuesta satisfactoria
   end
 
   test 'render a detailed product page' do
-    
+
   end
 
 end
